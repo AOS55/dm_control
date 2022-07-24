c8a3a7a7a6dd6528d40e2cdb4e992c678b549711
quessy <aq15777@bristol.ac.uk>
2022-07-24 11:17:17 +0100

    added obstacle class with better camera options

--- dm_control/locomotion/arenas/obstacles.py
+++ dm_control/locomotion/arenas/obstacles.py
@@ -1,83 +1,108 @@
 """Walled room with central constraint"""
 
 from dm_control import composer
+from dm_control.composer.observation import observable
 from dm_control.locomotion.arenas import assets as locomotion_arenas_assets
+import os
+
 import numpy as np
 
+
 _GROUNDPLANE_QUAD_SIZE = 0.25
+_TOP_CAMERA_DISTANCE = 7.0
+_TOP_CAMERA_Y_PADDING_FACTOR = 1.1
 
 
 class Obstacle(composer.Arena):
     
-    def _build(self, size=(8, 8), name='obstacle', aesthetic='default', reflectance=.2):
+    def _build(self, size=(11, 11), name='obstacle'):
         super()._build(name=name)
 
+        self._aesthetic = 'outdoor_natural'
         self._size = size
-        top_camera_y_padding_factor = 1.1
-        top_camera_distance = 100
-        self._mjcf_root.default.geom.rgba = [1, 1, 1, 1]
 
-        if aesthetic != 'default':
-            ground_info = locomotion_arenas_assets.get_ground_texture_info(aesthetic)
-            sky_info = locomotion_arenas_assets.get_sky_texture_info(aesthetic)
-            texturedir = locomotion_arenas_assets.get_texturedir(aesthetic)
-            self._mjcf_root.compiler.texturedir = texturedir
+        sky_info = locomotion_arenas_assets.get_sky_texture_info(self._aesthetic)
 
-            self._ground_texture = self._mjcf_root.asset.add(
-                'texture', name='aesthetic_texture', file=ground_info.file,
-                type=ground_info.type)
-            self._ground_material = self._mjcf_root.asset.add(
-                'material', name='aesthetic_material', texture=self._ground_texture,
-                texuniform='true')
-            self._skybox = self._mjcf_root.asset.add(
-                'texture', name='aesthetic_skybox', file=sky_info.file,
-                type='skybox', gridsize=sky_info.gridsize,
-                gridlayout=sky_info.gridlayout)
-        else:
-            self._ground_texture = self._mjcf_root.asset.add(
-                'texture',
-                rgb1=[.2, .3, .4],
-                rgb2=[.1, .2, .3],
-                type='2d',
-                builtin='checker',
-                name='groundplane',
-                width=200,
-                height=200,
-                mark='edge',
-                markrgb=[0.8, 0.8, 0.8])
-            self._ground_material = self._mjcf_root.asset.add(
-                'material',
-                name='groundplane',
-                texrepeat=[2, 2],  # Makes white squares exactly 1x1 length units.
-                texuniform=True,
-                reflectance=reflectance,
-                texture=self._ground_texture)
-            self._skybox_texture = self._mjcf_root.asset.add(
-                'texture', type='skybox', name='skybox', builtin='gradient',
-                rgb1=[.4, .6, .8], rgb2=[0, 0, 0], width=100, height=100)
+        # self._skybox = self._mjcf_root.asset.add(
+        #     'texture',
+        #     name='aesthetic_skybox',
+        #     file=os.path.join(locomotion_arenas_assets.get_texturedir(self._aesthetic), sky_info.file),
+        #     type='skybox',
+        #     gridsize=sky_info.gridsize,
+        #     gridlayout=sky_info.gridlayout
+        # )
+
+        self._ground_texture = self._mjcf_root.asset.add(
+            'texture',
+            rgb1=[0.2, 0.3, 0.4],
+            rgb2=[0.1, 0.2, 0.3],
+            type='2d',
+            builtin='checker',
+            name='groundplane',
+            width=200,
+            height=200,
+            mark='edge',
+            markrgb=[0.8, 0.8, 0.8]
+        )
+        
+
+        self._ground_material = self._mjcf_root.asset.add(
+            'material',
+            name='groundplane',
+            texrepeat=[2, 2],
+            texuniform=True,
+            reflectance=0.2,
+            texture=self._ground_texture
+        )
+
         self._ground_geom = self._mjcf_root.worldbody.add(
             'geom',
             type='plane',
             name='groundplane',
             material=self._ground_material,
-            size=list(size) + [_GROUNDPLANE_QUAD_SIZE])
+            # rgba=[1, 1, 1, 1],
+            size = list(size) + [_GROUNDPLANE_QUAD_SIZE],
+            pos = [0, 0, 0]
+        )
 
-        fovy_radians = 2 * np.arctan2(top_camera_y_padding_factor * size[1],
-                                  top_camera_distance)
+        #TODO: Work out why the ground doesn't render correctly 
+        ground_size = max(size)
+        top_camera_fovy = (360 / np.pi) * np.arctan2(
+            _TOP_CAMERA_Y_PADDING_FACTOR * ground_size / 2,
+            _TOP_CAMERA_DISTANCE)
         self._top_camera = self._mjcf_root.worldbody.add(
             'camera',
             name='top_camera',
-            pos=[0, 0, top_camera_distance],
-            quat=[1, 0, 0, 0],
-            fovy=np.rad2deg(fovy_radians))
+            pos=[0, 0, _TOP_CAMERA_DISTANCE],
+            zaxis=[0, 0, 1],
+            fovy = top_camera_fovy
+        )
+
+        # self._forward_camera = self._mjcf_root.worldbody.add(
+        #     'camera',
+        #     name='forward_camera',
+        #     pos=[0, 0, 7.0],
+        #     zaxis=[0, 0, 1],
+        #     fovy=150.0
+        # )
+
+    def _build_observables(self):
+        return ObstacleObservables(self)
 
     @property
     def ground_geoms(self):
         return (self._ground_geom,)
 
+    @property
+    def top_camera(self):
+        return self._top_camera
+
     def regenerate(self, random_state):
         pass
 
-    @property
-    def size(self):
-        return self._size
+
+class ObstacleObservables(composer.Observables):
+
+    @composer.observable
+    def top_camera(self):
+        return observable.MJCFCamera(self._entity.top_camera)
